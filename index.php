85488f98f782719406483d85b89b4c5a7a3d665a
Luthvihakim <luthvihakim05@gmail.com>
2024-11-21 15:17:56 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+$nama = 'Luthvi Hakim';
+$nim = '312310057';
+$kelas = 'TI.23CA1';
+
+
+?>
+
+<h2>Biodata Saya</h2>
+
+<p>
+Nama = <?= $nama ?><br>
+Nim = <?= $nim ?><br>
+Kelas = <?= $kelas ?>
+
+
+
+</p>
